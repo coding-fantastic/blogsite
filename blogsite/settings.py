2c2b1e6907be9a30308f6782432605e0770aafe0
coding-fantastic <alexnzioka@protonmail.com>
2022-03-13 00:58:27 +0300

    allowed host

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5%a8gdzd9%nd@eg1q128^8w23dmid-kic64xytyw*nuzq*&%+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'alexblogsite.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = [ 'alex2blogsite.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
